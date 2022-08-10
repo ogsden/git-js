98cdc153904430a35fe21898deee51ad9fca91fe
Denis <110948676+ogsden@users.noreply.github.com>
2022-08-10 21:14:06 +0300

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,5 +1,92 @@
 'use strict'
 
+// Рекурсия и стек
+
+// function sumTo(n) {
+//     let sum = 0;
+//     for (let i = 0; i <= n; i++){
+//         sum += n;
+//         n--;
+//     }
+//     return sum;
+// }
+
+// alert(sumTo(1));
+
+// function sumTo(n) {
+//     if (n == 1) {
+//         return n;
+//     } else {
+//         return n + sumTo(n-1);
+//     }
+// }
+
+// alert (sumTo(2));
+
+// function sumTo(n) {
+//     return (n * (n + 1)) / 2;
+// }
+
+// alert (sumTo(1));
+
+
+// function factorial (n) {
+//     if (n == 1) return n;
+//     return n * factorial(n - 1);
+// }
+
+// alert (factorial(5));
+
+
+// function fib(n) {
+//     let arr = [0, 1];
+
+//     for (let i = 2; i <= n; i++) {
+//         let prev = arr[i - 1];
+//         let prev2 = arr[i - 2];
+//         arr.push(prev + prev2);
+//     }
+
+//     return arr[n];
+// }
+
+// alert(fib(3)); // 2
+// alert(fib(7)); // 13
+// alert(fib(77));
+
+
+// function printList (list) {
+//     alert(list.value); 
+
+//     if (list.next) {
+//       printList(list.next); 
+//     }
+// }
+
+// let list = {
+//     value: 1,
+//     next: {
+//       value: 2,
+//       next: {
+//         value: 3,
+//         next: {
+//           value: 4,
+//           next: null
+//         }
+//       }
+//     }
+//   };
+
+// printList(list);
+
+
+// function printList (list) {
+//     if (list.next) {
+//         printList(list.next);
+//     }
+//     alert (list.value);
+// }
+
 // Замыкание
 
 // function sum(a) {
